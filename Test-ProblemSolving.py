d81c83f1c62db1f0d52ae78a8324360dbdd9ee5b
Andreyka <andreykostygin@gmail.com>
2025-10-20 12:07:54 +0300

    first commit

---
+++ Test-ProblemSolving.py
@@ -0,0 +1,68 @@
+
+x = 50
+a = 2
+b = 3
+age = 18
+password = "python1123"
+
+def firstProblem():
+    if x%2==0:
+        print("Число парне")
+    else:
+        print("Число непарне")
+
+def secondProblem():
+    if x == 0:
+        print("equals zero")
+    if x > 0:
+        print("more than 0")
+    if x<0:
+        print("less than 0")
+  
+def thirdProblem():
+    if age>=18:
+        print("Confirmed")
+    if age<18:
+        print("Denied")
+
+def fourthProblem():
+    if x <= 10 & x>= 1:
+        print("The right number")
+    else:
+        print("No it's not")
+
+def fifthProblem():
+    if password == "python123":
+        print("Password is correct")
+    else:
+        print("Password isn't correct")
+
+def sixthProblem():
+    if a > b:
+        print(a)
+    else:
+        print(b)
+
+
+def seventhProblem():
+    if x >= 90:
+        print("excelent")
+        return
+    if x >= 70:
+        print("good")
+        return
+    if x >= 50:
+        print("OK")
+        return
+    if x < 50:
+        print("Bad")
+        return
+
+seventhProblem()
+
+
+    
+
+
+
+
